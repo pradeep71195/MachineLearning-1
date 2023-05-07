bf50b13391c3dbc747095968588ca73317c1ed11
Pradeep <pradeep71195@gmail.com>
2023-05-07 19:36:34 +0530

    app.py and prediction in web form

--- src/utils.py
+++ src/utils.py
@@ -14,9 +14,16 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
-
     except Exception as e:
         logging.info("Exception occured at Save Object function")
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info("Exception occured at Load Object function")
         raise CustomException(e, sys)
 
 def evaluate_model(X_train, y_train, X_test, y_test, models):
